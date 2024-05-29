911dfbec32a782eebc56ba1cd9e37908dcc969f6
aflyingmole <167077766+aflyingmole@users.noreply.github.com>
2024-05-29 16:54:24 +0900

    gd

--- src/Main10871.java
+++ src/Main10871.java
@@ -3,25 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main10871 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = "";
-        StringTokenizer st = new StringTokenizer(str, " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-
-
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         for (int i = 0; i < n; i++) {
             if (arr[i] < x) {
-
+                System.out.print(arr[i] + " ");
             }
         }
-
     }
 }
